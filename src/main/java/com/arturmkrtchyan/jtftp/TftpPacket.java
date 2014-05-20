441be007d93378f9cf6e8987289d2fa354e0508d
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2014-05-20 16:36:12 +0200

    Added Packet comments

--- src/main/java/com/arturmkrtchyan/jtftp/TftpPacket.java
+++ src/main/java/com/arturmkrtchyan/jtftp/TftpPacket.java
@@ -21,23 +21,82 @@ public final class TftpPacket {
         this.errorMessage = errorMessage;
     }
 
-
+    /**
+     * ACK Packet
+     *
+     *   2 bytes     2 bytes
+     *  ---------------------
+     * | Opcode |   Block #  |
+     *  ---------------------
+     *
+     * @param blockNumber
+     * @return
+     */
     public static TftpPacket ack(final short blockNumber) {
         return new TftpPacket(Opcode.ACK, null, null, blockNumber, null, (short) 0, null);
     }
 
+    /**
+     * Error Packet
+     *
+     *   2 bytes     2 bytes      string    1 byte
+     *  -----------------------------------------
+     * | Opcode |  ErrorCode |   ErrMsg   |   0  |
+     *  -----------------------------------------
+     *
+     * @param errorCode
+     * @param errorMessage
+     * @return
+     */
     public static TftpPacket error(final short errorCode, final String errorMessage) {
         return new TftpPacket(Opcode.ERROR, null, null, (short) 0, null, errorCode, errorMessage);
     }
 
+    /**
+     * Data Packet
+     *
+     *   2 bytes     2 bytes      n bytes
+     *  ----------------------------------
+     * | Opcode |   Block #  |   Data     |
+     *  ----------------------------------
+     *
+     * @param blockNumber
+     * @param data
+     * @return
+     */
     public static TftpPacket data(final short blockNumber, final byte[] data) {
         return new TftpPacket(Opcode.DATA, null, null, blockNumber, data, (short) 0, null);
     }
 
+    /**
+     * RRQ Packet
+     *
+     *   2 bytes    string   1 byte     string   1 byte
+     *  -----------------------------------------------
+     * | 01/02 |  Filename  |   0  |    Mode    |   0  |
+     *  -----------------------------------------------
+     *
+     * @param fileName
+     * @param mode
+     * @return
+     */
     public static TftpPacket rrq(final String fileName, final TransferMode mode) {
         return new TftpPacket(Opcode.RRQ, mode, fileName, (short) 0, null, (short) 0, null);
     }
 
+    /**
+     *
+     * WRQ Packet
+     *
+     *   2 bytes    string   1 byte     string   1 byte
+     *  -----------------------------------------------
+     * | 01/02 |  Filename  |   0  |    Mode    |   0  |
+     *  -----------------------------------------------
+     *
+     * @param fileName
+     * @param mode
+     * @return
+     */
     public static TftpPacket wrq(final String fileName, final TransferMode mode) {
         return new TftpPacket(Opcode.WRQ, mode, fileName, (short) 0, null, (short) 0, null);
     }
